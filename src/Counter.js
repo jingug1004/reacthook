dbe30194967a99c05605dc53ebab236981a147fe
jingug1004 <jingug1004@gmail.com>
2019-09-21 11:13:32 +0900

    190921(토) 리액트를 다루는 기술_01 hook

---
+++ src/Counter.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Counter = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+      <div>
+        <p>
+          현재 카운터 값은 <b>{value}</b>입니다.
+        </p>
+        <button onClick={() => setValue(value + 1)}> + 1 </button>
+        <button onClick={() => setValue(value - 1)}> - 1 </button>
+      </div>
+  );
+}
+
+export default Counter;
---
+++ src/Info.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Info = () => {
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    }
+
+    const onChangeNickname = e => {
+        setNickname(e.target.value);
+    }
+
+    return (
+        <div>
+            <div>
+                <input vlaue={name} onChange={onChangeName}/>
+                <input vlaue={nickname} onChange={onChangeNickname}/>
+            </div>
+            <div>
+                <div>
+                    <b>이름:</b>{name}
+                </div>
+                <div>
+                    <b>닉네임:</b>{nickname}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Info;
